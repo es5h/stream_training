9c97fd310880aa3974c38c3b46e24d0789b43270
Eunseok Oh <lspk.lpg@gmail.com>
2021-12-03 01:49:14 +0900

    refactoring

--- src/train/Main.java
+++ src/train/Main.java
@@ -1,5 +1,5 @@
-package train
-import java.util.ArrayList;
+package train;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -7,21 +7,20 @@ import java.util.stream.IntStream;
 public class Main {
 
     private static int count(String number){
-        return 3 * number.length() - number.replaceAll("3","").length()
-                - number.replaceAll("6","").length()- number.replaceAll("9","").length();
+        return number.length() - number.replaceAll("3","").replaceAll("6","").replaceAll("9","").length();
     }
 
     private static String isClapped(Integer num){
         String number = num.toString();
 
         if(number.contains("3") || number.contains("6") || number.contains("9") )
-            return new String(new char[count(number)]).replace("\0", "짝");;
+            return new String(new char[count(number)]).replace("\0", "짝");
 
         return number;
     }
 
     public static void main(String[] args) {
-        List<String> list = IntStream.range(1, 334).mapToObj(x -> isClapped(x)).collect(Collectors.toList());
+        List<String> list = IntStream.range(1, 334).mapToObj(Main::isClapped).collect(Collectors.toList());
         System.out.println(list);
     }
 }
